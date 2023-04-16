0ffe4e1f60453cd1e4a9f4cf908daa96e47a2331
JonathanPC <jhoni161@gmail.com>
2023-04-15 21:41:27 -0500

    probando codewhisperer

---
+++ main.js
@@ -0,0 +1,2 @@
+//parce a csv string of song and return list (position, originalCharDate, artist, title)
+//ignore lines starting with #
--- pepe.php
+++ pepe.php
@@ -3,8 +3,17 @@
 <head>
     <meta charset="UTF-8" />
     <title>Pepe v1</title>
+
+<!--make a code for css style in H tag width in 20px and with shadow-->
+
 </head>
 <body>
     
+
+<?php
+
+
+
+?>
 </body>
 </html>
